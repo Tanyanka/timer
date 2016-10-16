8a459a0232880f95b9c44baaad94712c8a3a3ba8
Tanya <Chagovskaya>
2016-10-16 18:06:57 +0300

    Timer

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var timer = document.getElementById('timer');
+var toggleBtn = document.getElementById('start');
+var resetBtn = document.getElementById('reset');
+
+var watch = new Stopwatch('timer');
+
+function start () {
+    watch.start();
+    toggleBtn.textContent = 'Stop';
+}
+
+function stop () {
+    watch.stop();
+    toggleBtn.textContent = 'Start';
+}
+
+toggleBtn.addEventListener('click', function () {
+    (watch.isOn) ? start() : stop();
+});
+
+resetBtn.addEventListener('click', function () {
+    if (!watch.isOn) {
+        watch.reset();
+    }
+})
